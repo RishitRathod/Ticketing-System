8c1fad0a5db4f18df6973f96185d773617e8b2c4
aryan083 <aryanmahida2@gmail.com>
2024-06-06 11:09:39 +0530

    merging stuff

--- fetchEvents.php
+++ fetchEvents.php
@@ -1,25 +1,65 @@
 <?php 
 require_once 'db_connection.php';
 header('Content-Type: application/json');
-if($SERVER['REQUEST_METHOD']=='POST'){
+if($_SERVER['REQUEST_METHOD']=== 'POST'){
     $data = json_decode(file_get_contents('php://input'), true);
     $action=$data['action'];
     
     switch($action){
         case 'fetchPaginatedEventData':
-            $limit = $data['limit'];
+            try { $limit = $data['limit'];
             $offset = $data['offset'];
             $response=DB::fetchPaginatedEventData($limit, $offset);
             //add sucess message to response
-            if(!$response){
-                $response['success']=false;
-                $response['message']='No events found';
-                echo json_encode($response);
-                exit();
-            }
+            
             $response['success']=true;
             echo json_encode($response);
+        }catch(Exception $e){
+            echo json_encode(['success'=>false, 'message'=>$e->getMessage(),'data'=>$response]);
+        }
             break;
 
+        case 'FetchEventDetailsByOrgID':
+            try { $EventID = $data['EventID'];
+            $OrgID = $data['OrgID'];
+            $response=DB::FetchEventDetailsByOrgID($EventID,$OrgID);
+            
+            $response['success']=true;
+            echo json_encode($response);
+            }catch(Exception $e){
+                echo json_encode(['success'=>false, 'message'=>$e->getMessage(),'data'=>$response]);
+            }
+            break;
+
+        case 'FetchEventDetails':
+            try { $EventID = $data['EventID'];
+            $response=DB::FetchEventDetails($EventID);
+        
+            $response['success']=true;
+            echo json_encode($response);
+            }catch(Exception $e){
+                echo json_encode(['success'=>false, 'message'=>$e->getMessage(),'data'=>$response]);
+            }
+            break;
+        case 'FetchAllEventsByOrgID':
+           try { $OrgID = $data['OrgID'];
+            $response=DB::FetchAllEventsByOrgID($OrgID);
+            
+            $response['success']=true;
+            echo json_encode($response);
+            }catch(Exception $e){
+                echo json_encode(['success'=>false, 'message'=>$e->getMessage(),'data'=>$response]);
+            }
+            break;
+        case 'GetEventsByCatagory':
+            try { $Catagory = $data['Catagory'];
+            $response=DB::GetEventsByCatagory($Catagory);
+            $response['success']=true;
+            echo json_encode($response);
+            }catch(Exception $e){
+                echo json_encode(['success'=>false, 'message'=>$e->getMessage(),'data'=>$response]);
+            }
+            break;
+        
         }
 }
