26d46d24f43b7a3268f627e703e6badaed4527d5
aryan083 <aryanmahida2@gmail.com>
2024-06-06 11:12:58 +0530

    merging stuff

--- fetchEvents.php
+++ fetchEvents.php
@@ -7,59 +7,85 @@ if($_SERVER['REQUEST_METHOD']=== 'POST'){
     
     switch($action){
         case 'fetchPaginatedEventData':
-            try { $limit = $data['limit'];
-            $offset = $data['offset'];
-            $response=DB::fetchPaginatedEventData($limit, $offset);
-            //add sucess message to response
-            
-            $response['success']=true;
-            echo json_encode($response);
-        }catch(Exception $e){
+            try { 
+                $limit = $data['limit'];
+                $offset = $data['offset'];
+                $response=DB::fetchPaginatedEventData($limit, $offset);
+                //add sucess message to response
+                
+                $response['success']=true;
+                echo json_encode($response);
+            }
+            catch(Exception $e){
             echo json_encode(['success'=>false, 'message'=>$e->getMessage(),'data'=>$response]);
-        }
+            }
             break;
 
         case 'FetchEventDetailsByOrgID':
-            try { $EventID = $data['EventID'];
-            $OrgID = $data['OrgID'];
-            $response=DB::FetchEventDetailsByOrgID($EventID,$OrgID);
-            
-            $response['success']=true;
-            echo json_encode($response);
-            }catch(Exception $e){
+            try { 
+                $EventID = $data['EventID'];
+                $OrgID = $data['OrgID'];
+                $response=DB::FetchEventDetailsByOrgID($EventID,$OrgID);
+                
+                $response['success']=true;
+                echo json_encode($response);
+            }
+            catch(Exception $e){
                 echo json_encode(['success'=>false, 'message'=>$e->getMessage(),'data'=>$response]);
             }
             break;
 
         case 'FetchEventDetails':
-            try { $EventID = $data['EventID'];
-            $response=DB::FetchEventDetails($EventID);
+            try { 
+                $EventID = $data['EventID'];
+                $response=DB::FetchEventDetails($EventID);
         
-            $response['success']=true;
-            echo json_encode($response);
-            }catch(Exception $e){
+                $response['success']=true;
+                echo json_encode($response);
+            }
+            catch(Exception $e){
                 echo json_encode(['success'=>false, 'message'=>$e->getMessage(),'data'=>$response]);
             }
             break;
         case 'FetchAllEventsByOrgID':
-           try { $OrgID = $data['OrgID'];
-            $response=DB::FetchAllEventsByOrgID($OrgID);
-            
-            $response['success']=true;
-            echo json_encode($response);
-            }catch(Exception $e){
+           try { 
+                $OrgID = $data['OrgID'];
+                $response=DB::FetchAllEventsByOrgID($OrgID);
+                
+                $response['success']=true;
+                echo json_encode($response);
+            }
+            catch(Exception $e){
                 echo json_encode(['success'=>false, 'message'=>$e->getMessage(),'data'=>$response]);
             }
             break;
         case 'GetEventsByCatagory':
-            try { $Catagory = $data['Catagory'];
-            $response=DB::GetEventsByCatagory($Catagory);
-            $response['success']=true;
-            echo json_encode($response);
-            }catch(Exception $e){
+            try { 
+                $Catagory = $data['Catagory'];
+                $response=DB::GetEventsByCatagory($Catagory);
+                $response['success']=true;
+                echo json_encode($response);
+            }
+            catch(Exception $e){
                 echo json_encode(['success'=>false, 'message'=>$e->getMessage(),'data'=>$response]);
             }
             break;
+        case 'fetchPaginatedEventDataByOrgID':
+            try{
+                $limit = $data['limit'];
+                $offset = $data['offset'];
+                $OrgID = $data['OrgID'];
+                $response=DB::fetchPaginatedEventDataByOrgID($limit, $offset, $OrgID);
+                $response['success']=true;
+                echo json_encode($response);
+            }
+            catch(Exception $e){
+                echo json_encode(['success'=>false, 'message'=>$e->getMessage(),'data'=>$response]);
+            }
+            break;
+
+            default:
+                echo json_encode(['success'=>false, 'message'=>'Invalid action']);
         
         }
 }
